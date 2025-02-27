3be81ce5267990638adfdad20b20f9fa298f32d2
Alejandra Trejo <alext8005@yahoo.com>
2025-02-26 20:52:00 -0700

    added var christasday

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -9,7 +9,7 @@ namespace ChristmasWithTypes
         {
             var xmas = new Christmas();
 
-            var xmasDay = "Thursday";
+            var xmasDay = Christmas.Day.Thursday;
 
             //TODO set Santa's name to Kris Kringle
             xmas.Santa = "Kris Kringle";
